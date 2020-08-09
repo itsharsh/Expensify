9b1fed0271b46f08c8c4603edece23e2617947e2
Harsh <hkumarsingh142@gmail.com>
2020-08-10 02:14:12 +0530

    add: test cases for dispatch(add/edit expensepage)

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -4,33 +4,37 @@ import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
 import { editExpense, removeExpense } from "../actions/expenses";
 
-const EditExpensePage = (props) => (
-  <div>
-    <h1>Edit Expense Page</h1>
-    <ExpenseForm
-      expense={props.expense}
-      onSubmit={(expense) => {
-        props.dispatch(editExpense(props.expense.id, expense));
-        props.history.push("/");
-      }}
-    />
-    <button
-      onClick={() => {
-        props.dispatch(removeExpense({ id: props.expense.id }));
-        props.history.push("/");
-      }}
-    >
-      Remove
-    </button>
-  </div>
-);
-
-const mapStateToProps = (state, props) => {
-  return {
-    expense: state.expenses.find(
-      (expense) => expense.id === props.match.params.id
-    ),
+export class EditExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push("/");
   };
-};
 
-export default connect(mapStateToProps)(EditExpensePage);
+  onClick = () => {
+    this.props.removeExpense({ id: this.props.expense.id });
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Edit Expense Page</h1>
+        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+        <button onClick={this.onClick}>Remove</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find(
+    (expense) => expense.id === props.match.params.id
+  ),
+});
+
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: (id) => dispatch(removeExpense(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
